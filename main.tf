7964a5e972720016cb7734eacb6e0791a4a69e13
Emes101 <onyekaemes@gmail.com>
2026-07-09 01:33:35 -0400

    Refactor comments in main.tf for better readability
    Updated comments for clarity and formatting.

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
       version = "3.4.3"
     }
   }
+
   required_version = ">= 1.1.0"
 
   cloud {
@@ -62,13 +63,16 @@ resource "aws_instance" "web" {
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+
+  # connectivity to ubuntu mirrors is required to run
+  # apt-get update and apt-get install apache2
   egress {
     from_port   = 0
     to_port     = 0
